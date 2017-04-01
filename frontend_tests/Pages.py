9dd676d4f8033285f94d150ddbe718c04a2c1d3f
Deepak Bhaskaran <deepak@duo.com>
2017-04-01 13:48:40 -0400

    Added Travis CI and rog page tests

--- frontend_tests/Pages.py
+++ frontend_tests/Pages.py
@@ -24,6 +24,8 @@ class BasePage(object):
         else:
             return False
 
+    def check_on_right_page(self):
+        assert self.wd.current_url == self.url
 
 class LoginPage(BasePage):
     config = ConfigParser.ConfigParser()
@@ -62,8 +64,6 @@ class AdminDashboard(BasePage):
     url = 'https://dev.smartresponse.org' + '/dashboard'
     dashboard_identifier = (By.PARTIAL_LINK_TEXT, 'Dashboard')
 
-    def check_on_right_page(self):
-        assert self.wd.current_url == self.url
 
     def check_dashboard_link_shows_up(self):
         if self.wait_for_item_to_load(id_scheme=self.dashboard_identifier[0],
@@ -72,3 +72,48 @@ class AdminDashboard(BasePage):
         else:
             return False
 
+
+class RegistrationPage(BasePage):
+    url = 'https://dev.smartresponse.org' + '/registration'
+    org_name_identifier = (By.ID, 'org_name')
+    first_name_identifier = (By.ID, 'first_name')
+    last_name_identifier = (By.ID, 'last_name')
+    phone_number_identifier = (By.ID, 'phoneNumber')
+    email_identifier = (By.ID, 'email')
+    password_identifier = (By.ID, 'password')
+    password_confirm_identifier = (By.ID, 'passwordConfirmation')
+    auth_identifier = (By.ID, 'auth_checkbox')
+    recaptcha_identifier = (By.XPATH, '//div[contains(@class, "recaptcha-checkbox-checkmark")]')
+    add_organization_identifier = (By.XPATH, "//button[contains(text(),'Add Organization')]")
+
+    def click_add_organization(self):
+        elem = self.wd.find_element(*RegistrationPage.add_organization_identifier)
+        elem.click()
+        return AdminDashboard(self.wd)
+
+    def set_names(self, first_name, last_name):
+        elem = self.wd.find_element(*RegistrationPage.first_name_identifier)
+        elem.clear()
+        elem.send_keys(first_name)
+        elem = self.wd.find_element(*RegistrationPage.last_name_identifier)
+        elem.clear()
+        elem.send_keys(last_name)
+
+    def set_phone_number(self, phone_number):
+        elem = self.wd.find_element(*RegistrationPage.phone_number_identifier)
+        elem.clear()
+        elem.send_keys(phone_number)
+
+    def set_mail(self, email_id):
+        elem = self.wd.find_element(*RegistrationPage.email_identifier)
+        elem.clear()
+        elem.send_keys(email_id)
+
+    def set_password(self, password):
+        elem = self.wd.find_element(*RegistrationPage.password_confirm_identifier)
+        elem.clear()
+        elem.send_keys(password)
+
+
+class RegistrationSuccessPage(BasePage):
+    url = 'https://dev.smartresponse.org/registration-success'
